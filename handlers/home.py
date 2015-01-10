616cdc5b28c9213430af9b2ed147485a18ee786f
Luke Zulauf <lzulauf@gmail.com>
2015-01-09 22:09:41 -0800

    Removing python files
    These were from a bad directory merge

--- handlers/home.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-class BudgetHandler(webapp2.RequestHandler):
-  def get(self):
-    self.redirect("/build/web/budgeter.html", permanent=True)
-
-app = webapp2.WSGIApplication([
-  ("/", BudgetHandler),
-  ], debug=True)
--- helpers/import_utils.py
+++
@@ -1,8 +0,0 @@
-def import_non_local(name):
-  import imp, sys
-
-  f, pathname, desc = imp.find_module(name, sys.path[1:])
-  module = imp.load_module(name, f, pathname, desc)
-  f.close()
-
-  return module
